6f9fd1a83e0da13d62423f0cbdd76fce3d862e01
Ângelo Artur <ang.vianna@gmail.com>
2024-09-17 23:15:16 -0300

    server from pocket-js

---
+++ src/functions/create-goal.ts
@@ -0,0 +1,22 @@
+import { db } from "../db";
+import { goals } from "../db/schema";
+import type { CreateGoalRequest } from "../interfaces/create-goal-request.interface";
+
+export async function createGoal({
+  title,
+  desiredWeeklyFrequency,
+}: CreateGoalRequest) {
+  const result = await db
+    .insert(goals)
+    .values({
+      title,
+      desiredWeeklyFrequency,
+    })
+    .returning();
+
+  const goal = result[0];
+
+  return {
+    goal,
+  };
+}
---
+++ src/http/server.ts
@@ -0,0 +1,28 @@
+import { getWeekSummaryRoute } from './routes/get-week-summary';
+import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { createGoalRoute } from "./routes/create-goal";
+import { createCompletionRoute } from "./routes/create-completion";
+import { getPendingGoalsRoute } from "./routes/get-pending-goal";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createGoalRoute);
+app.register(createCompletionRoute);
+app.register(getPendingGoalsRoute);
+app.register(getWeekSummaryRoute);
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server Running");
+  });

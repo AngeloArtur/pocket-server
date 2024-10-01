df53a411443f395ea6633188c26c5d4449b6247c
Ângelo Artur <ang.vianna@gmail.com>
2024-09-30 22:33:17 -0300

    chore: using fastify latest version

--- src/http/server.ts
+++ src/http/server.ts
@@ -8,11 +8,16 @@ import {
 import { createGoalRoute } from "./routes/create-goal";
 import { createCompletionRoute } from "./routes/create-completion";
 import { getPendingGoalsRoute } from "./routes/get-pending-goal";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {
+    origin: '*'
+})
 
 app.register(createGoalRoute);
 app.register(createCompletionRoute);
